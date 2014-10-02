fb269247abdeebf124916456c985f35a95d8ce15
Evan You <yyx990803@gmail.com>
2014-10-02 03:26:24 -0400

    bump

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,20 @@
-<style>
-  html, body {
-    color: #666;
-    font-family: Helvetica, Arial, sans-serif;
-  }
+<style lang="stylus">
+font-stack = Helvetica, sans-serif
+primary-color = #333
+body
+  font 100% font-stack
+  color primary-color
 </style>
 
-<template>
-  <h1>This is the app</h1>
-  <div v-component="a"></div>
-  <div v-component="b"></div>
+<template lang="jade">
+h1 This is the app
+div(v-component="a")
+div(v-component="b")
 </template>
 
-<script>
-  module.exports = {
-    components: {
-      a: require('./components/a.vue'),
-      b: require('./components/b.vue')
-    }
-  }
+<script lang="coffee">
+module.exports =
+  components:
+    a: require './components/a.vue'
+    b: require './components/b.vue'
 </script>
