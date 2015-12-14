695a1766c0aad69d421a30daa126d013812e092f
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-12-14 16:30:46 +0100

    add two way data binding for the center and the zoom of the map

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,14 +1,107 @@
 /* vim: set softtabstop=2 shiftwidth=2 expandtab : */
 
 <template>
-  Hello this is working
+  <div class="vue-map-container">
+    <div class="vue-map"></div>
+    <slot> I have the default content</slot>
+  </div>
 </template>
 
 <script>
+
+  import {loaded} from '../manager.js'
+
   export default {
+    props: {
+      center: {
+        required: true,
+        twoWay: true,
+        type: Object
+      },
+      zoom: {
+        required: true,
+        twoWay: true,
+        type: Number
+      }
+    },
+    replace:false,
     data() {
-      console.log('hello world');
-      return {}
+      return {
+        mapObject : null,
+      }
+    },
+
+    ready () {
+      loaded.then(() => {
+        // getting the DOM element where to create the map
+        const element = this.$el.getElementsByClassName('vue-map')[0];
+
+        // creating the map
+        this.mapObject = new google.maps.Map(element, {
+          center: this.center,
+          zoom: this.zoom
+        });
+
+        /**
+         * Handling the 2-way data binding for the center of the map
+         */
+        var ignoreChanginCenter = 0;
+
+        this.$watch('center', () => {
+          ignoreChanginCenter--;
+          if (ignoreChanginCenter === -1) {
+            this.mapObject.setCenter(this.center);
+          }
+        }, {
+          deep: true,
+        });
+
+        this.mapObject.addListener('center_changed', () => {
+          if(ignoreChanginCenter == 0) {
+            const newCenter = this.mapObject.getCenter();
+            this.center = {
+              lat: newCenter.lat(),
+              lng: newCenter.lng()
+            }
+          }
+          ignoreChanginCenter++;
+        });
+
+        /**
+         * Handling the 2-way data binding for the zoom of the map
+         */
+        var ignoreChaningZoom = 0;
+
+        this.$watch('zoom', () => {
+          ignoreChaningZoom--;
+          if (ignoreChaningZoom === -1) {
+            this.mapObject.setZoom(this.zoom);
+          } 
+        });
+
+        this.mapObject.addListener('zoom_changed', () => {
+          if(ignoreChaningZoom === 0) {
+            this.zoom = this.mapObject.getZoom();
+          }
+          ignoreChaningZoom++;
+        });
+      });
     }
   }
 </script>
+
+<style lang="less">
+
+.full() {
+  width: 100%;
+  height:100%;
+}
+
+.vue-map-container {
+  .full();
+  .vue-map {
+    .full();
+  }
+}
+
+</style>
