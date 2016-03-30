533b9a0ae0f2b49e11199819aaf4d12938c876e0
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2016-03-30 11:13:50 +0200

    display an error message when the google places is not loaded for placeInputs

--- src/components/placeInput.vue
+++ src/components/placeInput.vue
@@ -79,6 +79,10 @@
         delete propsToBind.place;
         delete propsToBind.selectFirstOnEnter;
         propsBinder(this, this.autoCompleter, propsToBind);
+      }).catch(() => {
+        setTimeout(() => {
+          throw new Error("Impossible to load the Autocomplete Class from the google places api, did you loaded it ?");
+        }, 0);
       });
     },
 
